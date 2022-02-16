8da68bb90598af86f247f2b7267204be4b403fb5
Felicia Au Thersthol <feliciaau@hotmail.com>
2022-02-16 11:30:21 +0100

    Red

--- src/test/java/EmployeeSystemTest.java
+++ src/test/java/EmployeeSystemTest.java
@@ -1,4 +1,24 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class EmployeeSystemTest {
 
-    EmployeeSystem employeeSystem;
+    private EmployeeSystem employeeSystem;
+
+    EmployeeSystem testObject;
+
+    @BeforeEach
+    public void beforeEach(){
+        employeeSystem = new EmployeeSystem();
+    }
+
+    @Test
+    public void addNewEmployeeTest(){
+        String newEmployee = "Tim";
+        testObject.addNewEmployee();
+
+    }
 }
