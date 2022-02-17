893ba46bb6086d8bec1145ecf399295a7e800014
Felicia Au Thersthol <feliciaau@hotmail.com>
2022-02-17 18:04:54 +0100

    Green

--- src/main/java/EmployeeSystem.java
+++ src/main/java/EmployeeSystem.java
@@ -2,14 +2,17 @@ import java.util.ArrayList;
 
 public class EmployeeSystem {
 
-    private ArrayList<Employee> addNewEmployee = new ArrayList<>();
+    private ArrayList<Employee> employeeList = new ArrayList<>();
 
     public void addNewEmployee(Employee newEmployee){
-        this.addNewEmployee.add(newEmployee);
+        this.employeeList.add(newEmployee);
     }
 
     public ArrayList<Employee> getNewEmployee() {
-        return addNewEmployee;
+        return employeeList;
     }
 
+    public void removeEmployee(Employee newEmployee) {
+        this.employeeList.remove(newEmployee);
+    }
 }
--- src/test/java/EmployeeSystemTest.java
+++ src/test/java/EmployeeSystemTest.java
@@ -9,8 +9,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class EmployeeSystemTest {
 
-    private EmployeeSystem employeeSystem;
-
     EmployeeSystem testObject;
 
     @BeforeEach
@@ -22,26 +20,31 @@ public class EmployeeSystemTest {
     public void addNewEmployeeTest(){
         Employee newEmployee = new Employee("Tim","Tom",35,35000);
         testObject.addNewEmployee(newEmployee);
-        Employee newEmployee2 = new Employee("Samantha","Tester", 33, 40000);
-        testObject.addNewEmployee(newEmployee2);
-        Employee newEmployee3 = new Employee("Mary","Poppin", 40, 50000);
-        testObject.addNewEmployee(newEmployee3);
-        System.out.println(newEmployee.getID());
+        System.out.println(newEmployee.getID()); // alternativ newEmployed.forEach(value -> System.out.println(value.getFirstName()));
         ArrayList<Employee> newEmployed = testObject.getNewEmployee();
         assertEquals("Tim",newEmployed.get(0).getFirstName());
         assertEquals("Tom",newEmployed.get(0).getLastName());
         assertEquals(35,newEmployed.get(0).getAge());
         assertEquals(35000,newEmployed.get(0).getSalary());
-        //newEmployed.forEach(value -> System.out.println(value.getFirstName()));
+
     }
 
     @Test
     public void removeEmployeeTest(){
+        Employee newEmployee = new Employee("Samantha","Tester", 33, 40000);
+        testObject.addNewEmployee(newEmployee);
+        Employee newEmployee1 = new Employee("Mary","Poppin", 40, 50000);
+        testObject.addNewEmployee(newEmployee1);
+        Employee newEmployee2 = new Employee("Tim","Tom",35,35000);
+        testObject.addNewEmployee(newEmployee2);
+        System.out.println(" ");
+        System.out.println(newEmployee.getFirstName());
+        System.out.println(newEmployee1.getFirstName());
+        System.out.println(newEmployee2.getFirstName());
         ArrayList<Employee> newEmployed = testObject.getNewEmployee();
-        employeeSystem.removeEmployee(newEmployee);
-
-        //employeesFromSystem.forEach(value -> System.out.println(value.getFirstName()));
+        testObject.removeEmployee(newEmployee1);
+        assertEquals("Tim",newEmployed.get(1).getFirstName());
         }
     }
 
-}
+
